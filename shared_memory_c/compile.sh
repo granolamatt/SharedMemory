5ee0434118205bf86f6dd5ca76494589a1ca9f9f
granolamatt <granolamatt@gmail.com>
2014-07-05 07:08:19 +0900

    Added C++ Interface

--- shared_memory_c/compile.sh
+++ shared_memory_c/compile.sh
@@ -2,5 +2,6 @@
 
 
 gcc -Wall -Wno-write-strings -O2 main.c getopts.c shared.c -o memtest -lrt -lpthread || exit 1
+g++ -Wall -Wno-write-strings -O2 shared.c shared_memory.cpp -o memtest_cpp -lrt -lpthread || exit -1
 gcc -Wall -Wno-write-strings -O2 -shared -fPIC -I. -I/usr/local/jdk1.7.0_55/include -I/usr/local/jdk1.7.0_55/include/linux shared.c jni/sharedmemory_SharedMemory.c -o libsharedmemory.so -lrt -lpthread || exit 1
 cp libsharedmemory.so sharedmemory/
